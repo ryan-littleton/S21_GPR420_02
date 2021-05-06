59b8fd90d78db3947ed39e965da73eb2631fb82a
Ryan Littleton <ryan.littleton@mymail.champlain.edu>
2021-05-06 17:37:18 -0400

    C++ stuff added and http calls

---
+++ Project3/Source/Project3/Public/CubeActor.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "HttpActor.h"
+#include "CubeActor.generated.h"
+
+class HttpActor;
+
+UCLASS()
+class PROJECT3_API ACubeActor : public AActor
+{
+	GENERATED_BODY()
+
+		UPROPERTY(Category = Meshes, VisibleAnywhere)
+		//TSubobjectPtr<UStaticMeshComponent> CubeMesh;
+		UStaticMeshComponent* CubeMesh;
+
+public:
+	// Sets default values for this actor's properties
+	ACubeActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "HTTP")
+		TSubclassOf<AHttpActor> Universalis;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Text")
+		FText RetainerName;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Text")
+		int32 Price;
+
+	AHttpActor* NewActor;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
